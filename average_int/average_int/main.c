0843a985c4eb2caf756d9e4867af006e241ce01c
ToriNoUta <2358909811@qq.com>
2018-11-15 10:46:29 +0800

    提交代码

--- average_int/average_int/main.c
+++ average_int/average_int/main.c
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-
+//不使用（a+b）/2的方式计算两个数的平均数
+//使用位运算的方法：两数二进制中相同部分使用&运算求得，不同部分使用^运算求得
+//将相同部分的全部与不同部分的一半相加
 int average_int(const int num1_, const int num2_)	
 {
-	int result_ = (num1_&num2_)+(num1_^num2_)/2;
+	int result_ = (num1_&num2_)+((num1_^num2_)>>1);
 	return result_;
 }
 
