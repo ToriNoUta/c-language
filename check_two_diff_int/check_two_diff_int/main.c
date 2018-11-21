39fa1f53c677ce1090c13f6394d526a488e12a1d
ToriNoUta <2358909811@qq.com>
2018-11-21 22:50:20 +0800

    一个数组中只有两个数字是出现一次， 其他所有数字都出现了两次。

--- check_two_diff_int/check_two_diff_int/main.c
+++ check_two_diff_int/check_two_diff_int/main.c
@@ -1,32 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+#include<assert.h>
 int cmp_int(const void* num1_, const void* num2_)
 {
 	return (*(int*)num1_ - *(int*)num2_);
 }
-void check_two_diff_int(int* num_, int size_)
+void check_two_diff_int(int* num_,const int size_)
 {
-	int count_ = 0;
+	assert(num_);
 	int i_ = 0;
-	int x_ = 0;
-	int y_ = 0;
-	qsort(num_, size_, sizeof(int), cmp_int);
+	int des_[2] = {0};
+	int* pdes_ = des_;
+	qsort(num_, size_, sizeof(num_[0]), cmp_int);
 	for(i_ = 0; i_ < size_; i_++)
 	{
-		count_ ^= num_[i_];
-		if (i_ % 2 != 0 && count_ != 0)
+		if (num_[i_ - 1] != num_[i_] && num_[i_] != num_[i_ + 1])
 		{
-			x_ = num_[i_ - 1];
-			y_ = num_[i_];
+			*pdes_ = num_[i_];
+			pdes_++;
 		}
 	}
-	printf("不成对的两个数为：%d,%d\n", x_, y_);
+	printf("不成对的两个数为：%d,%d\n", des_[0], des_[1]);
 }
 
 int main()
 {
-	int num[] = { 1, 3, 4, 3, 1, 5, 6, 5, 7, 4 };
+	int num[] = { 1, 6, 4, 1, 3, 7, 6, 5, 7, 4 };
 	check_two_diff_int(num, sizeof(num) / sizeof(num[0]));
 	return 0;
 }
